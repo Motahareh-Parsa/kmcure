ffe9a57b9a06914f691077672c2bf810ce07bdd8
Motahareh-Parsa <75850472+Motahareh-Parsa@users.noreply.github.com>
2022-01-30 10:56:24 +0100

    Intercept in the penalized model is corrected
    Bug in deriving the intercept of the penalized model is fixed.
    Also, the BIC and AIC for the penalized model are provided.

--- R/penalized.R
+++ R/penalized.R
@@ -90,47 +90,34 @@ penalized <- function(time, event, survPreds, curePreds=NULL, R = 100, silent = 
 
     stdCovMat = cov(t(BootStdCoef))
 
-    timeS_lsa_lassoBIC = Sys.time()
-    stdCoefPenLassoBIC = suppressWarnings(LSAkmcure(stdCoef, stdCovMat, n,
-                                                    type = "lasso" , criteria = "BIC"))
-    timeE_lsa_lassoBIC = Sys.time()
+    # do LSAkmcure for lasso type shrinkage
+    timeS_lsa_lasso = Sys.time()
+    stdCoefPenLasso = suppressWarnings(LSAkmcure(stdCoef, stdCovMat, n, type = "lasso"))
+    timeE_lsa_lasso = Sys.time()
 
-    timeS_lsa_lassoAIC = Sys.time()
-    stdCoefPenLassoAIC = suppressWarnings(LSAkmcure(stdCoef, stdCovMat, n,
-                                                    type = "lasso" , criteria = "AIC"))
-    timeE_lsa_lassoAIC = Sys.time()
+    # do LSAkmcure for lar type shrinkage
+    timeS_lsa_lar = Sys.time()
+    stdCoefPenLar = suppressWarnings(LSAkmcure(stdCoef, stdCovMat, n, type = "lar"))
+    timeE_lsa_lar = Sys.time()
 
-    timeS_lsa_larBIC = Sys.time()
-    stdCoefPenLarBIC = suppressWarnings(LSAkmcure(stdCoef, stdCovMat, n,
-                                                  type = "lar" , criteria = "BIC"))
-    timeE_lsa_larBIC = Sys.time()
-
-    timeS_lsa_larAIC = Sys.time()
-    stdCoefPenLarAIC = suppressWarnings(LSAkmcure(stdCoef, stdCovMat, n,
-                                                  type = "lar" , criteria = "AIC"))
-    timeE_lsa_larAIC = Sys.time()
-
-    nonZeroStdCoefPenLassoBIC = (stdCoefPenLassoBIC != 0)
-    nonZeroStdCoefPenLassoAIC = (stdCoefPenLassoAIC != 0)
-    nonZeroStdCoefPenLarBIC = (stdCoefPenLarBIC != 0)
-    nonZeroStdCoefPenLarAIC = (stdCoefPenLarAIC != 0)
-
+    # prepare outlist for the penalized function
+    
     outlist = list()
 
-    outlist$isNonZeroCoef$lassoBIC = nonZeroStdCoefPenLassoBIC
-    outlist$isNonZeroCoef$lassoAIC = nonZeroStdCoefPenLassoAIC
-    outlist$isNonZeroCoef$larBIC = nonZeroStdCoefPenLarBIC
-    outlist$isNonZeroCoef$larAIC = nonZeroStdCoefPenLarAIC
+    outlist$isNonZeroCoef$lassoBIC = (stdCoefPenLasso$theta.bic != 0)
+    outlist$isNonZeroCoef$lassoAIC = (stdCoefPenLasso$theta.aic != 0)
+    outlist$isNonZeroCoef$larBIC = (stdCoefPenLar$theta.bic != 0)
+    outlist$isNonZeroCoef$larAIC = (stdCoefPenLar$theta.aic != 0)
 
-    outlist$coefPenalized$lassoBIC = stdCoefPenLassoBIC / scaleFull
-    outlist$coefPenalized$lassoAIC = stdCoefPenLassoAIC / scaleFull
-    outlist$coefPenalized$larBIC = stdCoefPenLarBIC / scaleFull
-    outlist$coefPenalized$larAIC = stdCoefPenLarAIC / scaleFull
+    outlist$coefPenalized$lassoBIC = stdCoefPenLasso$theta.bic / scaleFull
+    outlist$coefPenalized$lassoAIC = stdCoefPenLasso$theta.aic / scaleFull
+    outlist$coefPenalized$larBIC = stdCoefPenLar$theta.bic / scaleFull
+    outlist$coefPenalized$larAIC = stdCoefPenLar$theta.aic / scaleFull
 
-    outlist$stdCoefPenalized$lassoBIC = stdCoefPenLassoBIC
-    outlist$stdCoefPenalized$lassoAIC = stdCoefPenLassoAIC
-    outlist$stdCoefPenalized$larBIC = stdCoefPenLarBIC
-    outlist$stdCoefPenalized$larAIC = stdCoefPenLarAIC
+    outlist$stdCoefPenalized$lassoBIC = stdCoefPenLasso$theta.bic
+    outlist$stdCoefPenalized$lassoAIC = stdCoefPenLasso$theta.aic
+    outlist$stdCoefPenalized$larBIC = stdCoefPenLar$theta.bic
+    outlist$stdCoefPenalized$larAIC = stdCoefPenLar$theta.aic  
 
     outlist$ordinaryFit$stdCoef = stdCoef
     outlist$ordinaryFit$stdCovMat = stdCovMat
@@ -139,14 +126,17 @@ penalized <- function(time, event, survPreds, curePreds=NULL, R = 100, silent = 
 
     outlist$timeD_fit = fit$timeD
     outlist$timeD_bootstrap = difftime(timeE_boot, timeS_boot, units = "mins")
-    outlist$timeD_lsa_lassoBIC = difftime(timeE_lsa_lassoBIC, timeS_lsa_lassoBIC, units = "mins")
-    outlist$timeD_lsa_lassoAIC = difftime(timeE_lsa_lassoAIC, timeS_lsa_lassoAIC, units = "mins")
-    outlist$timeD_lsa_larBIC = difftime(timeE_lsa_larBIC, timeS_lsa_larBIC, units = "mins")
-    outlist$timeD_lsa_larAIC = difftime(timeE_lsa_larAIC, timeS_lsa_larAIC, units = "mins")
+    outlist$timeD_lsa_lasso = difftime(timeE_lsa_lasso, timeS_lsa_lasso, units = "mins")
+    outlist$timeD_lsa_lar = difftime(timeE_lsa_lar, timeS_lsa_lar, units = "mins")
 
     timeE = Sys.time()
     timeD = difftime(timeE, timeS, units = "mins")
     outlist$timeD = timeD
+
+    outlist$BIC_lasso = stdCoefPenLasso$best.bic
+    outlist$BIC_lar = stdCoefPenLar$best.bic
+    outlist$AIC_lasso = stdCoefPenLasso$best.aic
+    outlist$AIC_lar = stdCoefPenLar$best.aic
 
     if(!silent) cat("Running of the kmcure 'penalized' function is finished at", format(Sys.time(), "%H:%M:%S (%Y-%m-%d)."), "\n")
 
@@ -157,57 +147,88 @@ penalized <- function(time, event, survPreds, curePreds=NULL, R = 100, silent = 
 
 
 
-
-LSAkmcure = function (coef, covmat, n, type , criteria) {
-
+LSAkmcure = function (coef, covmat, n, type) {
+  
   type = tolower(type[1])
-  criteria = toupper(criteria[1])
-
+  
   ## split coefficients to gamma and beta parts based on the coef rownames
   names = rownames(coef)
   bintercept <- grepl("(Intercept)", names[1])
   betaintercept <- any(grepl("(Intercept)", names[-1]))
   bLength = sum(grepl("Gamma", names))
-
+  
   ### split coef and covmat to the b and beta parts and then do shrinkage using the lars.lsa function
-
+  
   # extract b part coef and it's coresponding covariance matrix
   bcoef = coef[1:bLength]
   bcov = covmat[1:bLength, 1:bLength]
   #dim(bcov)
-
+  
   # extract beta part coef and it's coresponding covariance matrix
   betacoef = coef[(1+bLength):length(coef)]
   betacov = covmat[(1+bLength):length(coef), (1+bLength):length(coef)]
-
+  
   bSI <- solve(bcov)
-
+  
   betaSI <- solve(betacov)
-
+  
   # ---- shrinkage b part using RobMixReg::lars.lsa
   bPart_lsa <- RobMixReg::lars.lsa(bSI, bcoef, bintercept, n, type)
-
-  bPart_ic = bPart_lsa$BIC
-  if(criteria=="AIC") bPart_ic = bPart_lsa$AIC
-
-  bPart_BestIndex = which.min(bPart_ic)
-  b0Pen = bPart_lsa$beta0[bPart_BestIndex]
-  bcoefPen = bPart_lsa$beta[bPart_BestIndex,]
-
+  
+  bExtract = extract_larslsa(l.fit = bPart_lsa, beta.ols=bcoef, intercept=bintercept, n=n)
+  
   # ---- shrinkage beta part using RobMixReg::lars.lsa
   betaPart_lsa <- RobMixReg::lars.lsa(betaSI, betacoef, betaintercept, n, type)
-
-  betaPart_ic = betaPart_lsa$BIC
-  if(criteria=="AIC") betaPart_ic = betaPart_lsa$AIC
-
-  betaPart_BestIndex = which.min(betaPart_ic)
-  betacoefPen = betaPart_lsa$beta[betaPart_BestIndex,]
-
-  thetaPen = c(b0Pen, bcoefPen, betacoefPen)
-
-  thetaPen = as.matrix(thetaPen)
-  rownames(thetaPen) = rownames(coef)
-  colnames(thetaPen) = "coef"
-
-  return(thetaPen)
+  
+  betaExtract = extract_larslsa(l.fit = betaPart_lsa, beta.ols=betacoef, intercept=betaintercept, n=n)
+  
+  # prepare objects for output
+  theta.ols = as.matrix(c(bExtract$beta.ols, betaExtract$beta.ols))
+  theta.bic = as.matrix(c(bExtract$beta.bic, betaExtract$beta.bic))
+  theta.aic = as.matrix(c(bExtract$beta.aic, betaExtract$beta.aic))
+  best.bic = c(bExtract$bestBIC, betaExtract$bestBIC)
+  best.aic = c(bExtract$bestAIC, betaExtract$bestAIC)
+  rownames(theta.ols) = rownames(coef); colnames(theta.ols) = "theta.ols"
+  rownames(theta.bic) = rownames(coef); colnames(theta.bic) = "theta.bic"
+  rownames(theta.aic) = rownames(coef); colnames(theta.aic) = "theta.aic"
+  names(best.bic)=c("GammaBestBIC","BetaBestBIC")
+  names(best.aic)=c("GammaBestAIC","BetaBestAIC")
+  LSAkmcure_outlist = list(theta.ols=theta.ols, theta.bic=theta.bic, theta.aic=theta.aic,
+                           best.bic=best.bic, best.aic=best.aic)
+  return(LSAkmcure_outlist)
 }
+
+
+
+extract_larslsa = function(l.fit, beta.ols, intercept, n){
+  t1 <- sort(l.fit$BIC, ind=T)
+  
+  t2 <- sort(l.fit$AIC, ind=T)
+  
+  beta <- l.fit$beta
+  
+  if(intercept) {
+    
+    beta0 <- l.fit$beta0+beta.ols[1]
+    
+    beta.bic <- c(beta0[t1$ix[1]],beta[t1$ix[1],])
+    
+    beta.aic <- c(beta0[t2$ix[1]],beta[t2$ix[1],])
+    
+  } else {
+    
+    beta0 <- l.fit$beta0
+    
+    beta.bic <- beta[t1$ix[1],]
+    
+    beta.aic <- beta[t2$ix[1],]
+    
+  }
+  bestBIC = l.fit$BIC[t1$ix[1]]
+  bestAIC = l.fit$AIC[t2$ix[1]]
+  obj <- list(beta.ols=beta.ols, 
+              beta.bic=beta.bic, beta.aic = beta.aic, 
+              bestBIC = bestBIC, bestAIC = bestAIC)
+  return(obj)
+}
+
